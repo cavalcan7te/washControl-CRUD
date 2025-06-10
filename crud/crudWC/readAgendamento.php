4fa52455167dd2b3e510e2468704732de348f90a
Eduarda Cavalcante <168551206+cavalcan7te@users.noreply.github.com>
2025-06-10 14:31:29 -0300

    read feito - 10/06

--- crud/crudWC/readAgendamento.php
+++ crud/crudWC/readAgendamento.php
@@ -1,46 +1,55 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ver agendamentos</title>
+    <title>Agendamentos</title>
 </head>
 <body>
-    <table>
-        <tr>
-            <th>
-                Nome
-            </th>
 
-            <th>
-                Telefone
-            </th>
+    <h2>Lista de Agendamentos</h2>
 
-            <th>
-                Placa
-            </th>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Telefone</th>
+                <th>Placa</th>
+                <th>Modelo do carro</th>
+                <th>Data do agendamento</th>
+                <th>Observações</th>
+                <th>Serviço</th>
+                <th>Status</th>
+            </tr>
+        </thead>
 
-            <th>
-                Modelo do carro
-            </th>
+        <tbody>
+            <?php
+                require_once "connection.php";
 
-            <th>
-                Data
-            </th>
+                $stmt = $conexao->prepare("SELECT * FROM agendamentos");
+                $stmt->execute();
 
-            <th>    
-                Observações
-            </th>
-
-            <th>
-                Serviço
-            </th>
-
-            <th>
-                Status
-            </th>
-
-        </tr>
+                while ($linha = $stmt->fetch(PDO::FETCH_OBJ)) {
+                    echo "<tr>";
+                    echo "<td>{$linha->nome}</td>";
+                    echo "<td>{$linha->telefone}</td>";
+                    echo "<td>{$linha->placa}</td>";
+                    echo "<td>{$linha->modelo}</td>";
+                    echo "<td>{$linha->data_agendamento}</td>";
+                    echo "<td>{$linha->observacoes}</td>";
+                    echo "<td>{$linha->servico}</td>";
+                    echo "<td>{$linha->status}</td>";
+                    echo "</tr>";
+                }
+            ?>
+        </tbody>
     </table>
+
+    <br>
+    <form action="agendar.php" method="post">
+        <input type="submit" value="Voltar">
+    </form>
+
 </body>
 </html>
